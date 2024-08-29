76e4337ed71e2acd2e6d18371d2377cd96f6521c
Schnedi3 <schnedi3@gmail.com>
2024-08-29 19:15:54 +0200

    Simplify theme switcher
    * I don't like to use inline style but
      in this case I had to use a couple lines inside "Header.tsx"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,49 @@
-import { useThemeSwitcher } from "../hooks/useThemeSwitcher";
+import { useTheme } from "../hooks/useTheme";
 
 import "../css/header.css";
 import "../css/themes.css";
 
 export const Header = () => {
-  const { currentTheme, setCurrentTheme } = useThemeSwitcher();
+  const { theme, setTheme } = useTheme();
 
   return (
     <section className="header_container">
       <h1>calc</h1>
-      <article className="themes_container">
+      <article className="themes">
         <h3>theme</h3>
-        <div className="slider_container">
-          <div className="labels">
-            <label htmlFor="default">1</label>
-            <label htmlFor="light">2</label>
-            <label htmlFor="violet">3</label>
+        <div className="buttons_slider">
+          <ol>
+            <button
+              className={theme === "theme-default" ? "active" : ""}
+              onClick={() => setTheme("theme-default")}
+            >
+              1
+            </button>
+            <button
+              className={theme === "theme-light" ? "active" : ""}
+              onClick={() => setTheme("theme-light")}
+            >
+              2
+            </button>
+            <button
+              className={theme === "theme-violet" ? "active" : ""}
+              onClick={() => setTheme("theme-violet")}
+            >
+              3
+            </button>
+          </ol>
+          <div className="slider">
+            <span
+              style={{
+                transform:
+                  theme === "theme-default"
+                    ? "translateX(-1.3rem)"
+                    : theme === "theme-light"
+                    ? "translateX(0)"
+                    : "translateX(1.3rem)",
+              }}
+            ></span>
           </div>
-          <input
-            type="radio"
-            id="default"
-            checked={currentTheme === "default"}
-            onChange={() => setCurrentTheme("default")}
-          />
-          <input
-            type="radio"
-            id="light"
-            checked={currentTheme === "light"}
-            onChange={() => setCurrentTheme("light")}
-          />
-          <input
-            type="radio"
-            id="violet"
-            checked={currentTheme === "violet"}
-            onChange={() => setCurrentTheme("violet")}
-          />
-          <div className="slider"></div>
         </div>
       </article>
     </section>
